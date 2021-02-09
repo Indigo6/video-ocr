60c706ecc55491b05a3724e126ebc7871b0b7125
Indigo6 <1401322857@qq.com>
2021-02-09 12:15:39 +0800

    recontruct project

--- ocrFrames.py
+++ ocrFrames.py
@@ -1,33 +1,18 @@
 import copy
 import shutil
+import sys
 import time
 import os
 import requests
-import base64
 
 import cv2 as cv
 import numpy as np
 
 from PIL import Image
-from utils import fmt_time
+from utils import fmt_time, ocr_baidu, get_access_token
 
 
-def ocr_baidu(image, ocr_token):
-    img = base64.b64encode(image)
-    request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
-    # request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
-    params = {"image": img}
-    request_url = request_url + "?access_token=" + ocr_token
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
-    ocr_response = requests.post(request_url, data=params, headers=headers)
-    if ocr_response:
-        words = ocr_response.json()['words_result']
-        result = ""
-        for item in words:
-            result += item['words']
-        return result
-    else:
-        return ""
+
 
 
 def ocr_paddle(img):
@@ -105,19 +90,14 @@ if __name__ == '__main__':
     if video_path.endswith(('.mp4', '.mkv', '.avi', '.rmvb')):
         if ocr_method == "online":
             # 百度 API 的 API key 和 Secret key
-            with open('baidu_keys.txt', mode='r') as f:
-                client_id = f.readline().strip()
-                client_secret = f.readline().strip()
-                # client_id 为官网获取的AK， client_secret 为官网获取的SK
-                host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&'\
-                       + 'client_id={}&client_secret={}'.format(client_id, client_secret)
-                response = requests.get(host)
-                if response:
-                    access_token = response.json()["access_token"]
-                    print(access_token)
-            get_frames(video_path, srt_color, ocr_method, output_type,
-                       language, clip_box, srt_prob_thres, change_prob_thres,
-                       access_token)
+            access_token = get_access_token('baidu_keys.txt')
+            if access_token is None:
+                print("Get token error!")
+                sys.exit()
+            else:
+                get_frames(video_path, srt_color, ocr_method, output_type,
+                           language, clip_box, srt_prob_thres, change_prob_thres,
+                           access_token)
         else:
             get_frames(video_path, srt_color, ocr_method, output_type,
                        language, clip_box, srt_prob_thres, change_prob_thres)
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import base64
+import requests
+
 
 # 用于对输出任务进度中的时间进行格式化
 def fmt_time(dtime):
@@ -10,3 +13,36 @@ def fmt_time(dtime):
     else:
         return '%d:%02d:%02d.%03d' % (int(dtime / 3600), int((dtime % 3600) / 60), int(dtime) % 60,
                                       int(dtime * 1000) % 1000)
+
+
+def get_access_token(key_file_path):
+    access_token = None
+    with open(key_file_path, mode='r') as f:
+        client_id = f.readline().strip()
+        client_secret = f.readline().strip()
+        # client_id 为官网获取的AK， client_secret 为官网获取的SK
+        host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&' \
+               + 'client_id={}&client_secret={}'.format(client_id, client_secret)
+        response = requests.get(host)
+        if response:
+            access_token = response.json()["access_token"]
+            print(access_token)
+    return access_token
+
+
+def ocr_baidu(image, ocr_token):
+    img = base64.b64encode(image)
+    request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
+    # request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
+    params = {"image": img}
+    request_url = request_url + "?access_token=" + ocr_token
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    ocr_response = requests.post(request_url, data=params, headers=headers)
+    if ocr_response:
+        words = ocr_response.json()['words_result']
+        result = ""
+        for item in words:
+            result += item['words']
+        return result
+    else:
+        return ""
